2ab47ee8500c4ea69b99604c8782b2d153bbf19a
zhenjess <jessicazhen8@gmail.com>
2019-11-26 12:13:37 -0800

    first initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const rootDiv = document.getElementById("root");
+    // ReactDOM.render(<h1>React is working</h1>, rootDiv);
+    rootDiv.innerHTML = "works"
+});
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js?$/,  // This will match either .js or .jsx
+            exclude: /node_modules/,
+            options: {
+                presets: ["@babel/preset-env"]
+            },
+        }]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    },
+    watch: true
+};
+
+
+

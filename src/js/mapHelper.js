6a9a30942084f4003c0614cba8111f3f6c49589c
zhenjess <jessicazhen8@gmail.com>
2019-11-27 11:12:44 -0800

    Set helper methods to world map

--- src/js/mapHelper.js
+++ src/js/mapHelper.js
@@ -1,12 +1,25 @@
-//sort data by country and records
+//sort data by country and record count
+//scale values and pass fill color to datamaps
 
-function mapData(records, year) {
-
-    //associate country with records of incidents
-    if (!year || year === "All-time" {
+function mapData(incidents, period) {
+    if (!period) {
         let dataset = d3.nest()
             .key(function(d) {
                 return d.victim.country[4];
             })
-    })
+            .rollup(function(v) {
+                return v.length;
+            })
+            .object(incidents.filter(function(d) {
+                return d.timeline.incident.year === period
+            }));
+
+            let minValue = d3.min(d3.values(dataset));
+            let maxValue = d3.max(d3.values(dataset));
+
+            let colorScale = d3.scaleBand.log()
+                .domain([minValue, maxValue])
+                .range(['#e0ba3a', '#e07a3a']);
+
+    }
 }

ac0e420510432a298d78f38c1945f32e795fefc4
zhenjess <jessicazhen8@gmail.com>
2019-11-30 22:20:58 -0800

    Add file for constant variables

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+export const ANIMATION_DURATION = 1000;
+export const ANIMATION_DELAY = 0;
+export const ANIMATION_EASING = d3.easePoly;
+export const HEIGHT = window.innerHeight - 300;
+export const WIDTH = window.innerWidth - 250;
+export const MARGINS = 100;
+
+

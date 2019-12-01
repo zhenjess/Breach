3ddbcb60ad3909e1e5e0f624bab09b59c837ba9f
zhenjess <jessicazhen8@gmail.com>
2019-11-30 23:29:06 -0800

    Filter Location

--- src/filter.js
+++ src/filter.js
@@ -15,7 +15,49 @@ class Filter {
             .style('border-radius', '7px')
             .style('padding', '2rem');
 
-        
+        d3.selectAll('.location')
+            .on('hoverOver', (d, i) => {
+                let className;
+                if (typeof d === 'string') {
+                    className = `location-${d.split(' ').join('-')}`;
+                } else if (d.data) {
+                    className = d.data.class;
+                } else {
+                    className = d.class;
+                }
+                const allOtherLocations = document.querySelectorAll(`.location:not(.${className})`);
+                allOtherLocations.forEach(data => {
+                    data.classList.add('filter');
+                })
+
+                // tooltip  
+                tooltip
+                    .style("opacity", 1)
+                    .style("stroke", "black")
+                    .html(
+                        `
+                            <div class="tooltip__inner">
+                                <p class="tooltip__city"><strong>Location: </strong><span>${
+                                        d.location
+                                        }</span></p>
+                                <p class="tooltip__detail"><strong>Breach Risk Score:</strong> <span>${
+                                        d.happinessScore
+                                        }/10</span></p>
+                            </div>
+                        `
+                    );
+                const mainHeader = document.querySelector('.main__header');
+                mainHeader.setAttribute('style', `color: ${d.color}`);
+            })
+            .on('noHover', () => {
+                const allLocationData = document.querySelectorAll('.location');
+                allLocationData.forEach(data => {
+                    data.classList.remove('filter');
+                })
+
+                const mainHeader = document.querySelector(".main__header");
+                mainHeader.setAttribute("style", `color: initial`);
+            })
     }
 }
 

7ae482af3845af1d51bee71198004be6c8d5a73f
zhenjess <jessicazhen8@gmail.com>
2019-12-09 10:49:13 -0800

    Create modal for chart description

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     const rootDiv = document.getElementById("root");
     // ReactDOM.render(<h1>React is working</h1>, rootDiv);
-    rootDiv.innerHTML = "works"
+    //rootDiv.innerHTML = "works"
+    //modal
+    const modalBg = document.querySelector('.modal-bg');
+    modalBg.addEventListener('click', handleModalBgClick);
 });
+
+//close modal
+function handleModalBgClick(e) {
+    
+}

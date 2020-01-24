8a85343bf9c9f15c8db306fd0d98dd12eb80ed30
zhenjess <jessicazhen8@gmail.com>
2020-01-23 22:23:45 -0800

    Intialize graph container and map x and y data range

--- src/graph.js
+++ src/graph.js
@@ -24,5 +24,25 @@ export const trendGraph = (selection, props) => {
     const innerWidth = width - margin.left - margin.right;
 
     //initialize graph container
-    
+    const g = selection.selectAll('.graph-container').data([null]);
+    const gEnter = g
+        .enter().append('g')
+            .attr('class', 'graph-container');
+
+    gEnter
+        .merge(g)
+            .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+    //map data x-range to graph range
+    const xScale = scaleLinear()
+        .domain(extent(data, xValue))
+        .range([0, innerWidth])
+        .nice();
+
+    //map data y-range to graph range
+    const yScale = scaleLinear()
+        .domain(extent(data, yValue).reverse())
+        .range([0, innerHeight])
+        .nice();
+
 }
